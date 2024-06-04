9d0bd3088092600ba91750df19b89e0509ce765b
Macey Lewis <danna.macey.lewis@gmail.com>
2024-06-04 14:48:12 -0400

    created components for images and installed reactstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
 import { useState } from 'react'
+import { Routes, Route } from "react-router-dom"
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { ImageGallery } from './components/images/ImageGallery'
 
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
-    <>
-        <div className="Dashboard">
-      <header className="App-header">
-        <h1>Welcome to Nutshell</h1>
-      </header>
-    </div>
-    </>
-  )
+  return <Routes>
+    <Route path="/images" element={<ImageGallery />} />
+  </Routes>
 }
 
 export default App
