8c8913968c43da2b831c391e7c92e97e0edcd984
Chelsea Brooks <cbrooks6212@gmail.com>
2024-06-05 11:05:49 -0400

    merged

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,25 @@ import { Outlet, Route, Routes } from "react-router-dom";
 import { Articles } from "./componenets/articles/articles";
 import { NewArticle } from "./componenets/articles/newArticle";
 import { TaskEventContainer } from "./componenets/TaskEventContainer.jsx";
+import { Login } from "./componenets/auth/Login";
+import { Authorized } from "./views/Authorized";
+import { ApplicationViews } from "./views/applicationViews";
+import { Register } from "./componenets/auth/Register";
 
-
-function App() {
+export const App = () => {
   const [count, setCount] = useState(0);
 
   return (
     <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+
       <Route
-        path="/"
+        path="*"
         element={
-          <>
-            <Navbar />
-            <Outlet />
-          </>
+          <Authorized>
+            <ApplicationViews />
+          </Authorized>
         }
       >
         <Route path="articles" element={<Articles />} />
@@ -29,6 +34,4 @@ function App() {
       </Route>
     </Routes>
   );
-}
-
-export default App;
+};
---
+++ src/views/applicationViews.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+import { Outlet, Route, Routes } from "react-router-dom";
+import { Articles } from "../componenets/articles/articles";
+import { NewArticle } from "../componenets/articles/newArticle";
+import { Navbar } from "../componenets/navbar/navbar";
+
+export const ApplicationViews = () => {
+    const [currentUser, setCurrentUser] = useState({});
+
+    useEffect(() => {
+      const localNutshellUser = localStorage.getItem("nutshell_user");
+      const nutshellUserObject = JSON.parse(localNutshellUser);
+      setCurrentUser(nutshellUserObject);
+    }, []);
+
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <>
+            <Navbar />
+            <Outlet />
+          </>
+        }
+      >
+        <Route path="articles" element={<Articles currentUser={currentUser}/>} />
+        <Route path="newArticle" element={<NewArticle currentUser={currentUser}/>} />
+      </Route>
+    </Routes>
+  );
+};
